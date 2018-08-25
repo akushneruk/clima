063e723ed98af4bc25f63f83f425934bbb795990
Andriy Kushneruk <kushneruk.andriy@gmail.com>
2018-08-25 23:50:19 +0300

    Update  output sht

--- bin/sht3_044.py
+++ bin/sht3_044.py
@@ -5,5 +5,5 @@ sensor = SHT31(address = 0x44)
 degrees = sensor.read_temperature()
 humidity = sensor.read_humidity()
 
-print 'Temp             = {0:0.3f} deg C'.format(degrees)
-print 'Humidity         = {0:0.2f} %'.format(humidity)
+print('Temp             = {0:0.3f} deg C'.format(degrees))
+print('Humidity         = {0:0.2f} %'.format(humidity))
