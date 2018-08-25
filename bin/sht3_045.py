d745ce9df43452d4149a37884667178cf9b7a784
Andriy Kushneruk <kushneruk.andriy@gmail.com>
2018-08-26 00:27:51 +0300

    Remove trash

--- bin/sht3_045.py
+++
@@ -1,9 +0,0 @@
-from Adafruit_SHT31 import *
-
-sensor = SHT31(address = 0x45)
-
-degrees = sensor.read_temperature()
-humidity = sensor.read_humidity()
-
-print('Temp             = {0:0.3f} deg C'.format(degrees))
-print('Humidity         = {0:0.2f} %'.format(humidity))
