14058aff5ceabd354d7ebdfcc836e9cc23d087d1
Andriy Kushneruk <kushneruk.andriy@gmail.com>
2018-08-28 22:01:41 +0000

    Update file

--- bin/gpio.py
+++ bin/gpio.py
@@ -1,28 +1,13 @@
 #!/usr/bin/env python3
 
 import RPi.GPIO as GPIO
+
 GPIO.setmode(GPIO.BCM)
-channel_1 = 6
-channel_2 = 13
-channel_3 = 19
-channel_4 = 26
+GPIO.setwarnings(False)
+channel = [6, 13, 19, 26]
+
+for index in range(len(channel)):
+    	GPIO.setup(channel[index], GPIO.OUT)
+	index+= 1
 
 
-GPIO.setup(channel_1, GPIO.OUT)
-GPIO.setup(channel_2, GPIO.OUT)
-GPIO.setup(channel_3, GPIO.OUT)
-GPIO.setup(channel_4, GPIO.OUT)
-
-# Setup your channel
-#GPIO.setup(channel, GPIO.OUT)
-#GPIO.output(channel, GPIO.LOW)
-
-# To test the value of a pin use the .input method
-channel_is_on = GPIO.input(channel_1)  # Returns 0 if OFF or 1 if ON
-print(channel_is_on)
-channel_is_on = GPIO.input(channel_2)
-print(channel_is_on)
-channel_is_on = GPIO.input(channel_3)
-print(channel_is_on)
-channel_is_on = GPIO.input(channel_4)
-print(channel_is_on)
